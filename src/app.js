97a068d87cd94824ace3975324b789d49294e6fb
Sahnawaz Husain <sahnwaz643786@gmail.com>
2025-03-24 19:05:13 +0530

    Login End Point/Route and password encryption done

--- src/app.js
+++ src/app.js
@@ -1,141 +1,189 @@
 console.log("Building DEV Tinder");
-const express=require("express");
-const connectDB =require("./config/database");
+const express = require("express");
+const connectDB = require("./config/database");
 const { UserModel } = require("./models/user");
-const app=express();
+const { validateSignupData } = require("./utils/validation");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY="sAHzEESHAN@123"
 
+const app = express();
 app.use(express.json());
 //signup route
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
+  try {
+    //user info fetch from body
+    const { firstName, lastName, password, email,role } = await req.body;
 
-    // console.log(req.body);
-    // console.log(req);
+    //validate signup data by function define in "./utils/validation.js"
+    validateSignupData(req);
 
-    //---------------------------DUMMY DATA FILLING IN DB --------------------
-    // const userDummy={
-    //     firstname:"Ajad",
-    //     ladtName:"Khan",
-    //     email:"ajad@gmail.com",
-    //     password:"AjadAjad",
-    //     age:21,
-    //     gender:"Male"
-    // };
-   
-console.log(req.body);
-const User=req.body; //user from Post request store in User then with the help of userModel created new user
-const user=new UserModel(User); //create new user with the help of UserModel(input); 
-// creating new instence of userModel
-// now put it in database
-try{
-    await user.save(); 
+    //Encrypt the password
+    console.log(password); //normal password
+    const hashPassword = await bcrypt.hash(password, 10); //encrypted password
+    console.log(hashPassword);
+
+    //data inserting in database
+    const user = await new UserModel({
+      firstName,
+      lastName,
+      email,
+      role,
+      password: hashPassword,
+    });
+    //data saving in dataBase
+    await user.save(user);
+
     res.status(200).json({
-        msg:"new user created"
-    })
-}catch(err){
+      msg: "new user created",
+    });
+  } catch (err) {
     console.log("Error in saving");
-    res.status(401).send("error in saving user"+ err.message);
-}
+    res.status(401).send("error:" + err.message);
+  }
+});
 //-------------------------------------------------------------------------------------
- });
-//get user by email
-app.get("/user",async (req,res)=>{
-
-    try{
-        const userEmail=await req.body.email;
-        const foundUser=await UserModel.findOne({email:userEmail});
-        if(foundUser.length===0){
-            res.status(404).send("user Not found");
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password} = await req.body;
+    //sanitize the data
+    const emailResponse = validator.isEmail(email);
+    if (!emailResponse) {
+      //email ha ki nahi
+      throw new Error("Enter email");
+    } else {
+      //find user with email in database
+      const userResponse = await UserModel.findOne({ email: email });
+      if (userResponse) {
+        //means user found
+        //now check password is same or not
+        console.log(userResponse.password);
+        const role=userResponse.role;//role feetch from db
+        console.log(role);
+        const hashPassword = userResponse.password;//fetch hash pass from db
+        const passwordResponse = await bcrypt.compare(password, hashPassword);//compair
+        if (passwordResponse) {
+          const token=jwt.sign({
+            role
+          },SECRET_KEY)
+          res.status(200).send("user Login and generated Token:"+ token);
         }else{
-            res.status(201).send(foundUser);
-            console.log(foundUser)
+          res.send("Invalid credentials")
         }
-    }catch(err){
-        console.log("something went wrong while serarching user by emial");
-        res.status(401).send("something went wrong",err);
+      }else{
+        res.status(404).send("invalid credentials");
+      }
     }
-})
-
+  } catch (err) {
+    res.send(err);
+  }
+});
+//get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = await req.body.email;
+    const foundUser = await UserModel.findOne({ email: userEmail });
+    if (foundUser.length === 0) {
+      res.status(404).send("user Not found");
+    } else {
+      res.status(201).send(foundUser);
+      console.log(foundUser);
+    }
+  } catch (err) {
+    console.log("something went wrong while serarching user by emial");
+    res.status(401).send("something went wrong" + err);
+  }
+});
 
 // FEED API, get request to /feed Route
-app.get("/feed",async (req,res)=>{
-    try{
-        const allUser=await UserModel.find({});
-        console.log(allUser)
-        res.status(201).send(allUser)
-    }catch(err){
-        console.log(err);
-        res.status(404).send("user Not found")
-    }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const allUser = await UserModel.find({});
+    console.log(allUser);
+    res.status(201).send(allUser);
+  } catch (err) {
+    console.log(err);
+    res.status(404).send("user Not found");
+  }
+});
 
 //find by id
-app.get("/id",async (req,res)=>{
-    try{
-        const user=await UserModel.findById({_id:"67de3a3745f827248439beb3"});
-        if(user){
-            res.json({
-                user
-            })
-        }else{
-            res.send("user not found with this id")
-        }
-    }catch(err){
-        res.status(404).send("something went wrong")
+app.get("/id", async (req, res) => {
+  try {
+    const user = await UserModel.findById({ _id: "67de3a3745f827248439beb3" });
+    if (user) {
+      res.json({
+        user,
+      });
+    } else {
+      res.send("user not found with this id");
     }
-})
+  } catch (err) {
+    res.status(404).send("something went wrong");
+  }
+});
 
 //update and delete API
-app.delete("/user",async (req,res)=>{
-    const userId=await req.body.userId;
-    try{
-        const response=await UserModel.findByIdAndDelete(userId);
-        console.log(response);
-        if(response){
-            res.status(201).send("find and user deleted")
-        }
-    }catch(err){
-        res.send("something went wrong!")
+app.delete("/user", async (req, res) => {
+  const userId = await req.body.userId;
+  try {
+    const response = await UserModel.findByIdAndDelete(userId);
+    console.log(response);
+    if (response) {
+      res.status(201).send("find and user deleted");
     }
-})
-app.patch("/user",async (req,res)=>{
-    const userId=await req.body.userId;
-    const data=await req.body;
+  } catch (err) {
+    res.send("something went wrong!");
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = await req.body.userId;
+  const data = await req.body;
 
-   
-    try{
-        const AllowedUpdates=["userId","ImageUrl","skill","about","gender","age","password"];
-        const isUpdateAllows=Object.keys(data).every((k)=>
-        AllowedUpdates.includes(k)
-        );
-        if(data?.skill.length>10){
-            throw new Error("Skill is not more than 10");
-        }
-        if(!isUpdateAllows){
-            throw new Error("Update not allowed")
-        }
-
-        const response=await UserModel.findByIdAndUpdate({_id:userId},data,{
-            returnDocument:"after",
-            runValidators:true
-        });
-                                        //userID and data
-        if(response){
-            res.status(200).send("user Updated sucessfully");
-        }else{
-            res.send("err in deleting user")
-        }
-
-    }catch(err){
-        res.send("something went wrong:::"+err.message)
+  try {
+    const AllowedUpdates = [
+      "userId",
+      "ImageUrl",
+      "skill",
+      "about",
+      "gender",
+      "age",
+      "password",
+    ];
+    const isUpdateAllows = Object.keys(data).every((k) =>
+      AllowedUpdates.includes(k)
+    );
+    if (data?.skill.length > 10) {
+      throw new Error("Skill is not more than 10");
     }
-})
+    if (!isUpdateAllows) {
+      throw new Error("Update not allowed");
+    }
+
+    const response = await UserModel.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    //userID and data
+    if (response) {
+      res.status(200).send("user Updated sucessfully");
+    } else {
+      res.send("err in deleting user");
+    }
+  } catch (err) {
+    res.send("something went wrong:::" + err.message);
+  }
+});
 
 //database connection
-connectDB().then(()=>{
-    console.log("data base is connected")
-    app.listen(3000,()=>{
-        console.log("app is listening on port:3000")
-    })
-}).catch((err)=>{
+connectDB()
+  .then(() => {
+    console.log("data base is connected");
+    app.listen(3000, () => {
+      console.log("app is listening on port:3000");
+    });
+  })
+  .catch((err) => {
     console.err("database is not connected sucessfully");
-})
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,64 +1,75 @@
-
-const mongoose=require("mongoose");
-const validator=require("validator");
+const mongoose = require("mongoose");
+const validator = require("validator");
 //create user schema
-const userSchema= mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        minLength:4,
-        maxLemgth:50
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: 4,
+      maxLemgth: 50,
     },
-    lastName:{
-        type:String,
-        required:true
+    lastName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        unique:true,
-        required:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email is not valid"+value)
-            }
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid" + value);
         }
+      },
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    age:{
-        type:String
+    age: {
+      type: String,
     },
-    gender:{
-        type:String,
-        //search from internet
-        validate(value){
-            if(!["male","female","other"].includes(value)){
-                throw new Error("Gender data is not valid")
-            }
+    gender: {
+      type: String,
+      //search from internet
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
         }
+      },
     },
-    aboout:{
-        type:String,
-        default:"This is default value of about you"
+    aboout: {
+      type: String,
+      default: "This is default value of about you",
     },
-    skill:{
-        type:[String]
+    skill: {
+      type: [String],
     },
-    ImageUrl:{
-        type:String,
-        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm-TruksPXPI5imDL_kfzEfFiAZwg5AzHtWg&s"
-    }
-},{
-    timestamps:true,
-})
+    ImageUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm-TruksPXPI5imDL_kfzEfFiAZwg5AzHtWg&s",
+    },
+    role:{
+      type:String,
+      validate(value) {
+        if (!["Admin", "User"].includes(value)) {
+          throw new Error("Must fill admin or user");
+        }
+      }
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 //create user model
-const UserModel=mongoose.model("User",userSchema);
+const UserModel = mongoose.model("User", userSchema);
 //export
-module.exports={
-    UserModel
-}
+module.exports = {
+  UserModel,
+};
